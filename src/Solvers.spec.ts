5f74b5fa7697eb8e4187ac0d90cdfee91aad3b7b
Ryan Buchmeier <ryan.b@verytechnology.com>
2023-12-23 23:50:51 -0700

    Day 1 (Part 2)

--- src/Solvers.spec.ts
+++ src/Solvers.spec.ts
@@ -24,4 +24,42 @@ describe('Solvers', () => {
       expect(solver('1234\n5678\n9012')).toBe('164');
     });
   });
+
+  describe('Day 2', () => {
+    const solver = solvers[2];
+    test('two1nine', () => {
+      expect(solver('two1nine')).toBe('29');
+    });
+    test('eightwothree', () => {
+      expect(solver('eightwothree')).toBe('83');
+    });
+    test('abcone2threexyz', () => {
+      expect(solver('abcone2threexyz')).toBe('13');
+    });
+    test('xtwone3four', () => {
+      expect(solver('xtwone3four')).toBe('24');
+    });
+    test('4nineeightseven2', () => {
+      expect(solver('4nineeightseven2')).toBe('42');
+    });
+    test('zoneight234', () => {
+      expect(solver('zoneight234')).toBe('14');
+    });
+    test('7pqrstsixteen', () => {
+      expect(solver('7pqrstsixteen')).toBe('76');
+    });
+    test('full example input', () => {
+      expect(
+        solver(
+          'two1nine\n' +
+            'eightwothree\n' +
+            'abcone2threexyz\n' +
+            'xtwone3four\n' +
+            '4nineeightseven2\n' +
+            'zoneight234\n' +
+            '7pqrstsixteen'
+        )
+      ).toBe('281');
+    });
+  });
 });
--- src/Solvers.ts
+++ src/Solvers.ts
@@ -17,5 +17,73 @@ const solvers: Record<number, (input?: string) => string> = {
     }
     return '';
   },
+  2: (input) => {
+    if (!input) {
+      return '';
+    }
+    const lines = input.split('\n');
+    return String(
+      lines.reduce((total, line) => {
+        const firstValue = findValueOfFirstMatch(line);
+        const lastValue = findValueOfLastMatch(line);
+        return total + Number(firstValue + lastValue);
+      }, 0)
+    );
+  },
 };
+
+const numberMap: Record<string, string> = {
+  one: '1',
+  two: '2',
+  three: '3',
+  four: '4',
+  five: '5',
+  six: '6',
+  seven: '7',
+  eight: '8',
+  nine: '9',
+};
+
+export const findValueOfFirstMatch = (str: string) => {
+  const searchStrings = [
+    ...Object.keys(numberMap),
+    ...Array.from(Array(10).keys()).map((n) => (n + 1).toString()),
+  ];
+  return searchStrings.reduce(
+    (acc: { value: string; index: number }, searchString) => {
+      const index = str.indexOf(searchString);
+      if (index === -1) {
+        return acc;
+      }
+      const value = numberMap[searchString] || searchString;
+      if (acc.index === -1 || index < acc.index) {
+        return { value, index };
+      }
+      return acc;
+    },
+    { value: '', index: -1 }
+  ).value;
+};
+
+export const findValueOfLastMatch = (str: string) => {
+  const searchStrings = [
+    ...Object.keys(numberMap),
+    ...Array.from(Array(10).keys()).map((n) => (n + 1).toString()),
+  ];
+  return searchStrings.reduce(
+    (acc: { value: string; index: number }, searchString) => {
+      const index = str.lastIndexOf(searchString);
+      if (index === -1) {
+        return acc;
+      }
+      const value = numberMap[searchString] || searchString;
+      if (acc.index === -1 || index > acc.index) {
+        return { value, index };
+      }
+      return acc;
+    },
+    { value: '', index: -1 }
+  ).value;
+};
+
 export default solvers;
